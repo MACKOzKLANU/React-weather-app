6f8dbbcc7599045641e59ebf40b8d35cbe496119
MACKOzKLANU <sek-maciek@o2.pl>
2025-03-02 15:38:31 +0100

    https instead of http

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 export const fetchWeather = async (city = "LODZ") => {
     const API_KEY = "77950a756080419794b91033252602"
-    const BASE_URL = "http://api.weatherapi.com/v1"
+    const BASE_URL = "https://api.weatherapi.com/v1"
     try {
         let request = await fetch(`${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=3&aqi=no&alerts=no`, {
             method: 'GET', // Optional, as GET is the default method
